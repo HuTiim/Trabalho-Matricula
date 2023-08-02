f1c1d4fdda64c4be0bb7f3ce2af1ba03145baa5c
Danilo <danilo11franca@yahoo.com>
2023-08-02 18:46:48 -0300

    ajuste identação

--- CadastroAluno.js
+++ CadastroAluno.js
@@ -6,22 +6,22 @@ const SENSACHOCRIVEL=require ('readline-sync').question;
 let i=1
 
 function Cadastrar() {
-    while(Cadastrar){
+    while(true){
         Nome=SENSACHOCRIVEL("Nome:")
         Matricula=SENSACHOCRIVEL("Matricula:")
         parseInt(Idade=SENSACHOCRIVEL("Idade:"))
         parseInt(Nota=SENSACHOCRIVEL("Nota:"))
 
-let registro=([`${i};${Nome};${Matricula};${Idade};${Nota}`])    
-let texto=registro+"\n"
-SENSACHOCRIVELWRITE("BancoDeDados.csv",texto.toString(),{flag:"a"})
-i++
+        let registro=([`${i};${Nome};${Matricula};${Idade};${Nota}`])    
+        let texto=registro+"\n"
+        SENSACHOCRIVELWRITE("BancoDeDados.csv",texto.toString(),{flag:"a"})
+        i++
 
-   n=parseInt(SENSACHOCRIVEL("1 p/ Novo Cadastro 2 p/parar:"))
+        n=parseInt(SENSACHOCRIVEL("1 p/ Novo Cadastro 2 p/parar:"))
 
-   if (n!=1) {
-        break        
-    }    
+        if (n!=1) {
+                break        
+            }    
     }
 }
 Cadastrar()
